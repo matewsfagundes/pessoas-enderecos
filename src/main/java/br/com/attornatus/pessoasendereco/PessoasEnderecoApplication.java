488669ef640c22dd51d2a586546706583c898733
Matheus Fagundes <matewsfagundes@gmail.com>
2023-05-08 18:55:52 -0300

    inicia o projeto

---
+++ src/main/java/br/com/attornatus/pessoasendereco/PessoasEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.pessoasendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/pessoasendereco/PessoasEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.pessoasendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
