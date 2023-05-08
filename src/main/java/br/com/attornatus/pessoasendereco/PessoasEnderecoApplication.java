994191a45dbd6795a993b581b3bc590e100099c4
Matheus Fagundes <matewsfagundes@gmail.com>
2023-05-08 19:38:15 -0300

    implementa API de teste

--- src/main/java/br/com/attornatus/pessoasendereco/PessoasEnderecoApplication.java
+++ src/main/java/br/com/attornatus/pessoasendereco/PessoasEnderecoApplication.java
@@ -2,12 +2,21 @@ package br.com.attornatus.pessoasendereco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PessoasEnderecoApplication {
 
+	@GetMapping
+	public String getHomeTeste() {
+		return "Gerenciador pessoas e endereços – API Home";
+			
+		}
 	public static void main(String[] args) {
 		SpringApplication.run(PessoasEnderecoApplication.class, args);
 	}
-
 }
